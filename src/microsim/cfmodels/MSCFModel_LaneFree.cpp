trafficfluid-dssl/trafficfluid-sim

commit cbb7cd6f02944568d41cd75000155dbba57a340f

resolve vehicle placement issue for global control in intersection scenarios

src/microsim/cfmodels/MSCFModel_LaneFree.cpp
@@ -962,7 +962,7 @@ NumericalID* lf_plugin_get_all_neighbor_ids_front(NumericalID veh_id, double fro
 	
 	// find the current edge's index on the route array
 	int my_edge_index{ veh_route->edge_index(&(lfveh->get_vehicle()->getLane()->getEdge())) };
-	
+
 	// check whether edge was found
 	if (my_edge_index == -1) {
 		// last edge object pointer
@@ -1061,7 +1061,7 @@ NumericalID* lf_plugin_get_all_neighbor_ids_back(NumericalID veh_id, double back
 
 	// Get route object of vehicle
 	const MSRoute* veh_route = &lfveh->get_vehicle()->getRoute();
-
+	
 	// This returns the set of edges corresponding to the vehicle's path, including the internal ones (on junctions)
 	const ConstMSEdgeVector veh_edges = veh_route->getEdgeswInternal();
 
@@ -1091,7 +1091,7 @@ NumericalID* lf_plugin_get_all_neighbor_ids_back(NumericalID veh_id, double back
 
 	}
 
-	// this will be filled with the following edges starting from the current one
+	// this will be filled with the following edges starting from the current one // TODO consider removing this (and other occurences)
 	ConstMSEdgeVector all_veh_edges; //will contain the internal edges as well
 	for (int i = my_edge_index; i < veh_edges.size(); i++) {
 		all_veh_edges.push_back(veh_edges.at(i));
@@ -1111,7 +1111,7 @@ NumericalID* lf_plugin_get_all_neighbor_ids_back(NumericalID veh_id, double back
 	int found = binary_search_find_index(sorted_vehs, 0, (int)(size_edge - 1), veh_id, x_vid);
 
 	if (found == -1) {		
-		std::cout << "\nVehicle " << lfveh->get_vehicle()->getID() << " not found in sorted vector of edge " << veh_edges[my_edge_index]->getID() << "!\n";
+		std::cout << "\nVehicle " << lfveh->get_vehicle()->getID() << " not found in sorted vector of edge " << veh_edges[my_edge_index]->getID() << ","<< lfveh->get_vehicle()->getLane()->getEdge().getID() <<"," << x_vid << ","<< get_position_x(sorted_vehs->at(0)->getNumericalID()) <<"," << sorted_vehs->at(0)->getID() << "!\n";
 		*neighbors_size = 0;
 		return NULL;
 	}
@@ -3007,7 +3007,7 @@ LaneFreeSimulationPlugin::get_all_neighbors_internal(MSLaneFreeVehicle* lfveh, c
 
 	size_t found{ veh_index };//binary_search_find_index(sorted_vehs, 0, (int)(size_edge - 1), lfveh->get_vehicle()->getNumericalID(), x_vid);
 
-	if (found == -1) {		
+	if (found == -1) {
 		std::cout << "\nVehicle " << lfveh->get_vehicle()->getID() << " not found in sorted vector of edge " << veh_edges[my_edge_index]->getID() << "!\n";
 		return;
 	}
@@ -3069,7 +3069,7 @@ LaneFreeSimulationPlugin::get_all_neighbors_internal(MSLaneFreeVehicle* lfveh, c
 				my_edge_index--;
 			}
 
-			if (!cross_edge || my_pos < -(front_back)*distance || my_edge_index >= veh_edges.size() || my_edge_index < 0) {
+			if (!cross_edge || my_pos < -(front_back)*distance || my_edge_index >= veh_edges.size() || my_edge_index < 0) { 
 				// terminating condition
 				break;
 			}
@@ -3488,7 +3488,7 @@ LaneFreeSimulationPlugin::free_hashmap(){
 void
 LaneFreeSimulationPlugin::insert_vehicle(MSVehicle* veh){
 	NumericalID edge_id = veh->getLane()->getEdge().getNumericalID();
-	if (!veh->getLane()->getEdge().getExcludeFromMetrics()){		
+	if (!veh->getLane()->getEdge().getExcludeFromMetrics()){
 		myTotalVehicleCountWithExcluded++;
 	}
 	VehicleMapEdges::iterator it = allVehiclesMapEdges.find(edge_id);
@@ -3678,10 +3678,8 @@ LaneFreeSimulationPlugin::find_stored_edge(MSVehicle* veh){
 
 void
 LaneFreeSimulationPlugin::change_edge(MSVehicle* veh){
-	//std::cout << "Start with change "<< veh->getID()<<" !\n";
 	NumericalID new_edge_id = veh->getLane()->getEdge().getNumericalID();
 	NumericalID old_edge_id = find_stored_edge(veh);
-	//std::cout << "old edge " << old_edge_id << " new edge " << new_edge_id << " with name:"<< veh->getLane()->getEdge().getID() <<"\n";
 	//std::cout << " new lane " << veh->getLane()->getID() << "\n";
 	if(old_edge_id==new_edge_id){
 		return;
@@ -3695,10 +3693,12 @@ LaneFreeSimulationPlugin::change_edge(MSVehicle* veh){
 
 		if(old_edge_excluded){
 			//std::cout << "Old edge excluded\n";
+			//std::cout << "veh " << veh->getID() << " added in TTS excluded\n";
 			myTotalVehicleCountWithExcluded++;
 		}
 
 		if(new_edge_excluded){
+			//std::cout << "veh " << veh->getID() << " removed from TTS excluded\n";
 			//std::cout << "New edge excluded\n";
 			myTotalVehicleCountWithExcluded--;
 		}
@@ -3770,7 +3770,7 @@ LaneFreeSimulationPlugin::remove_vehicle(MSVehicle* veh){
 	VehicleMapEdges::iterator it = allVehiclesMapEdges.find(edge_id);
 
 	if (!vehlane->getEdge().getExcludeFromMetrics()){
-		
+		//std::cout << "veh " << veh->getID() << " removed from TTS excluded\n";
 		myTotalVehicleCountWithExcluded--;
 	}
 	
@@ -3840,13 +3840,13 @@ LaneFreeSimulationPlugin::convert_to_local_coordinates(double* x_pos_local, doub
 
 
 	*x_pos_local = mylane->interpolateGeometryPosToLanePos(mylane->getShape().nearest_offset_to_point25D(pos, false));
-
-	double angle_lane = mylane->getShape().beginEndAngle();
-	const double perpDist = mylane->getShape().distance2D(pos, false);
-	*y_pos_local = perpDist;// std::min(perpDist, 0.5 * (mylane->getWidth() + myveh->getVehicleType().getWidth() - MSGlobals::gLateralResolution));
-	PositionVector tmp = mylane->getShape();
-	tmp.move2side(-*y_pos_local); // moved to left
-	if (tmp.distance2D(pos) > perpDist) {
+	
+	double angle_lane = mylane->getShape().beginEndAngle();	
+	const double perpDist = mylane->getShape().distance2D(pos, false);	
+	*y_pos_local = perpDist;// std::min(perpDist, 0.5 * (mylane->getWidth() + myveh->getVehicleType().getWidth() - MSGlobals::gLateralResolution));	
+	PositionVector tmp = mylane->getShape();	
+	tmp.move2side(-*y_pos_local); // moved to left	
+	if (tmp.distance2D(pos) > perpDist) {		
 		*y_pos_local = -*y_pos_local;
 	}
 

src/microsim/MSRoute.h
@@ -111,7 +111,7 @@ public:
         if (it == myEdgeswInternal.end()) {
             return -1;
         }
-        return it - myEdgeswInternal.begin();
+        return (int)(it - myEdgeswInternal.begin());
     }
 
     int edge_index_normal(const MSEdge* const edge) const {
@@ -119,7 +119,7 @@ public:
         if (it == myEdges.end()) {
             return -1;
         }
-        return it - myEdges.begin();
+        return (int)(it - myEdges.begin());
     }
 
     const ConstMSEdgeVector& getEdgeswInternal() const {

src/microsim/MSVehicle.cpp
@@ -1138,6 +1138,11 @@ MSVehicle::getSlope() const {
 
 Position
 MSVehicle::getPosition(const double offset) const {
+    // LFPlugin Begin
+    if (getVehicleType().getParameter().cmdModel == SUMO_TAG_LF_CMD_BICYCLE && getGlobalCoordinatesControl()) {
+        return cachedGlobalPos;
+    }
+    // LFPlugin End
     if (myLane == nullptr) {
         // when called in the context of GUI-Drawing, the simulation step is already incremented
         if (myInfluencer != nullptr && myInfluencer->isRemoteAffected(MSNet::getInstance()->getCurrentTimeStep())) {
@@ -1893,7 +1898,7 @@ MSVehicle::joinTrainPartFront(MSVehicle* veh) {
         }
         const double newLength = myType->getLength() + veh->getVehicleType().getLength();
         getSingularType().setLength(newLength);
-        assert(myLane == veh->getLane());
+        assert(myLane == veh->getLane());        
         myState.myPos = veh->getPositionOnLane();
         myStops.begin()->joinTriggered = false;
         return true;
@@ -3676,7 +3681,7 @@ MSVehicle::processLaneAdvances(std::vector<MSLane*>& passedLanes, bool& moved, s
                         // avoid skipping stop due to numerical instability
                         // this is a special case for rail vehicles because they
                         // continue myLFLinkLanes past stops
-                        approachedLane = myLane;
+                        approachedLane = myLane;                        
                         myState.myPos = myLane->getLength();
                         break;
                     }
@@ -4645,7 +4650,7 @@ MSVehicle::enterLaneAtMove(MSLane* enteredLane, bool onTeleporting) {
             double new_x, new_y;
             LaneFreeSimulationPlugin::getInstance()->convert_to_local_coordinates(&new_x, &new_y, cachedGlobalPos, myLane);
             myState.myPos = new_x;
-            myState.myPosLat = new_y;
+            myState.myPosLat = new_y;            
             //std::cout << "veh:" << getID() << " proceeds to lane:" << myLane->getID() << " with new pos:" << cachedGlobalPos.x() << "," << cachedGlobalPos.y() << "\n";
         }
         // LFPlugin End
@@ -4653,7 +4658,7 @@ MSVehicle::enterLaneAtMove(MSLane* enteredLane, bool onTeleporting) {
 
     } else {
         // normal move() isn't called so reset position here. must be done
-        // before calling reminders
+        // before calling reminders        
         myState.myPos = 0;
         myCachedPosition = Position::INVALID;
         activateReminders(MSMoveReminder::NOTIFICATION_TELEPORT, enteredLane);
@@ -5558,7 +5563,11 @@ MSVehicle::setBlinkerInformation() {
     } else {
         const MSLane* lane = getLane();
         std::vector<MSLink*>::const_iterator link = MSLane::succLinkSec(*this, 1, *lane, getBestLanesContinuation());
-        if (link != lane->getLinkCont().end() && lane->getLength() - getPositionOnLane() < lane->getVehicleMaxSpeed(this) * (double) 7.) {
+        if (link != lane->getLinkCont().end() && lane->getLength() - getPositionOnLane() < lane->getVehicleMaxSpeed(this) * (double) 7.
+            // LFPlugin Begin
+            && (!getGlobalCoordinatesControl())
+            // LFPlugin End
+            ) {
             switch ((*link)->getDirection()) {
                 case LinkDirection::TURN:
                 case LinkDirection::LEFT:

src/microsim/MSVehicle.h
@@ -418,12 +418,12 @@ public:
         cachedGlobalPos.setz(getPosition().z());
     }
 
-    Position& getCachedGlobalPos() {
+    Position& getCachedGlobalPos(){
         return cachedGlobalPos;
     }
 
     
-    bool getGlobalCoordinatesControl() {
+    bool getGlobalCoordinatesControl() const{
         return global_coordinates;
     }
 

src/microsim/MSVehicleTransfer.cpp
@@ -135,6 +135,7 @@ MSVehicleTransfer::checkInsertions(SUMOTime time) {
                 desc.myVeh->workOnIdleReminders();
                 if (!desc.myVeh->signalSet(MSVehicle::VEH_SIGNAL_BLINKER_LEFT | MSVehicle::VEH_SIGNAL_BLINKER_RIGHT)) {
                     // signal wish to re-enter the road
+                    // keep temp comment to appear in changes
                     desc.myVeh->switchOnSignal(MSGlobals::gLefthand ? MSVehicle::VEH_SIGNAL_BLINKER_RIGHT : MSVehicle::VEH_SIGNAL_BLINKER_LEFT);
                     if (pa) {
                         // update freePosition so other vehicles can help with insertion

src/microsim/cfmodels/MSCFModel_LaneFree.h
@@ -229,25 +229,21 @@ public:
         
         accel_y = uy;
         accel_x = ux;
-
-        
         
     }
 
     void apply_acceleration_bicycle(double F, double delta) {
-
         // no need to define more parameters
         accel_x = F;
         accel_y = delta;
 
-        
     }
 
     // Complies with the CF model, returns the next speed of the vehicle. Lateral position is updated internally here
     double apply_acceleration_internal(){
         if (myveh->getVehicleType().getParameter().cmdModel == SUMO_TAG_LF_CMD_BICYCLE) {
-            apply_acceleration_internal_bicycle();
-            myveh->setMyAccelerationBC(accel_x);
+            apply_acceleration_internal_bicycle();            
+            myveh->setMyAccelerationBC(accel_x);            
             myveh->setMyDeltaBC(accel_y);
         }
         else {
@@ -403,11 +399,17 @@ protected:
         v_next = v_cur + F * TS;
 
         bool global_coordinates = myveh->getGlobalCoordinatesControl();
-        //printf("curr step local:\n x:%f,y:%f\n", get_position_x()+ (sigma / 2) * cos(theta_cur), get_position_y() + (sigma / 2) * sin(theta_cur));
+        
+        //printf("F:%f\tdelta:%f\n", F, delta);
+        //double theta_cur_relative = myveh->getAngleRelative();
+        //printf("curr step local\t:x:%f,y:%f\n", get_position_x()+ (sigma / 2) * cos(theta_cur_relative), get_position_y() + (sigma / 2) * sin(theta_cur_relative));
+                
         if (global_coordinates) {
             get_global_coordinates_bicycle_model(&x_cur_back, &y_cur_back, sigma, theta_cur);
+            
             //Position pos_cur = myveh->getCachedGlobalPos();
-            //printf("cached pos:(%f,%f) vs obtained pos:(%f,%f)", pos_cur.x() - (sigma)*cos(theta_cur), pos_cur.y() - (sigma)*sin(theta_cur), x_cur_back, y_cur_back);
+            //printf("cached pos:\t(%f,%f)\n", pos_cur.x(), pos_cur.y());
+            
             //printf("local (front):(%f,%f)\n",myveh->getPositionOnLane() ,myveh->getLateralPositionOnLane());
             //x_cur_back = pos_cur.x() - (sigma)*cos(theta_cur);
             //y_cur_back = pos_cur.y() - (sigma)*sin(theta_cur);
@@ -452,7 +454,10 @@ protected:
             const MSLane* mylane = myveh->getLane();
             //std::cout << "mylane:" << mylane->getID() << "\n";
             convert_to_local_coordinates(&x_next_front, &y_next_front, cachedGlobalPos, mylane);
-            //std::cout << "next local:(" <<x_next_front<< "," << y_next_front<< ")\n";
+            
+            //printf("next cached pos:\t(%f,%f)\n", cachedGlobalPos.x(), cachedGlobalPos.y());
+            //std::cout << "next local:(" << x_next_front << "," << y_next_front << ")\n";
+            
         }
         //printf("next step local (front):\n x:%f,y:%f\n", x_next_front, y_next_front);
         
@@ -463,6 +468,8 @@ protected:
         deltaPos_x_front = x_next_front - myveh->getPositionOnLane();
         // vehicles are not allowed to move backwards     
         // The following line creates issues for global coordinates control   
+        
+        
         //deltaPos_x_front = (deltaPos_x_front < 0 ) ? 0 : deltaPos_x_front;
         //std::cout << "delta x: (local)" << deltaPos_x_front << "\n";
         

src/microsim/devices/MSDevice_Tripinfo.cpp
@@ -64,6 +64,11 @@ SUMOTime MSDevice_Tripinfo::myTotalDelayTime(0);
 SUMOTime MSDevice_Tripinfo::myTotalDurationNoNeg(0);
 long long MSDevice_Tripinfo::myTotalVehicleCount(0);
 long long MSDevice_Tripinfo::myTotalVehicleCountExcludeEdges(0);
+//Made for ad-hoc log files on time-delay and tts
+//FILE* MSDevice_Tripinfo::fp_time_delay = fopen("time_delay.txt", "w");
+//FILE* MSDevice_Tripinfo::fp_time_delay_excluded = fopen("time_delay_excluded.txt", "w");
+//FILE* MSDevice_Tripinfo::fp_num_of_vehs = fopen("num_of_vehs.txt", "w");
+//FILE* MSDevice_Tripinfo::fp_num_of_vehs_excluded = fopen("num_of_vehs_excluded.txt", "w");
 // LFPlugin End
 int MSDevice_Tripinfo::myWalkCount(0);
 double MSDevice_Tripinfo::myTotalWalkRouteLength(0);
@@ -150,6 +155,12 @@ MSDevice_Tripinfo::cleanup() {
     myTotalDurationNoNeg = 0;
     myTotalVehicleCount = 0;
     myTotalVehicleCountExcludeEdges = 0;
+
+    //Made for ad-hoc log files on time-delay and tts
+    /*fclose(fp_time_delay);
+    fclose(fp_time_delay_excluded);
+    fclose(fp_num_of_vehs);
+    fclose(fp_num_of_vehs_excluded);*/
     // LFPlugin End
     myWalkCount = 0;
     myTotalWalkRouteLength = 0;
@@ -313,7 +324,15 @@ MSDevice_Tripinfo::generateOutput(OutputDevice* tripinfoOut) const {
     myTotalTimeLossNoNeg += fmax(timeLoss, 0);// timeLossNoNeg;
     myTotalTimeLossExcludeEdges += timeLossExcludeEdges;
     myTotalTimeLossNoNegExcludeEdges += fmax(timeLossExcludeEdges, 0);//timeLossNoNegExludeEdges;
+    
+    //Made for ad-hoc log files on time-delay and tts
 
+    //fprintf(fp_time_delay, "%f,%f,%s\n", STEPS2TIME(timeLoss), SIMTIME, static_cast<MSVehicle&>(myHolder).getRoute().getID().c_str());
+   
+    
+    //fflush(fp_time_delay);
+    //fprintf(fp_time_delay_excluded, "%f,%f,%s\n", STEPS2TIME(timeLoss), SIMTIME, static_cast<MSVehicle&>(myHolder).getRoute().getID().c_str());
+    //fflush(fp_time_delay_excluded);
     myTotalExpectedTime += expectedTime;
     const SUMOTime myDelay = duration - expectedTime;
 

src/microsim/devices/MSDevice_Tripinfo.h
@@ -79,10 +79,16 @@ public:
 
     // LFPlugin Begin
     static void updateMyTotalVehicleCount(long long count) {
+        //Made for ad-hoc log files on time-delay and tts
+        /*fprintf(fp_num_of_vehs, "%lld\n", count);
+        fflush(fp_num_of_vehs);*/
         myTotalVehicleCount += count;
     }
 
     static void updateMyTotalVehicleCountExcludeEdges(long long count) {
+        //Made for ad-hoc log files on time-delay and tts
+        /*fprintf(fp_num_of_vehs_excluded, "%lld\n", count);
+        fflush(fp_num_of_vehs_excluded);*/
         myTotalVehicleCountExcludeEdges += count;
     }
     // LFPlugin End
@@ -293,6 +299,11 @@ private:
     static SUMOTime myTotalDurationNoNeg;
     static long long myTotalVehicleCount;
     static long long myTotalVehicleCountExcludeEdges;
+    //Made for ad-hoc log files on time-delay and tts
+    /*static FILE* fp_time_delay;
+    static FILE* fp_time_delay_excluded;
+    static FILE* fp_num_of_vehs;
+    static FILE* fp_num_of_vehs_excluded;*/
     // LFPlugin End
 
     static int myWalkCount;

commit aec841104137f862bcae7cfb935254ed4f9c394a

resolve issue for get_global_position_x, get_global_position_y caused by previous commit cbb7cd6f on global coordinates

src/microsim/cfmodels/MSCFModel_LaneFree.cpp
@@ -1966,7 +1966,7 @@ double lf_plugin_get_global_position_x(NumericalID veh_id) {
 	double global_pos_x;
 
 	if (lfveh->get_vehicle()->getGlobalCoordinatesControl()) {
-		global_pos_x = lfveh->get_vehicle()->getCachedGlobalPos().x();
+		global_pos_x = lfveh->get_vehicle()->getCachedGlobalPos().x() - (lfveh->get_vehicle()->getLength() / 2) * cos(lfveh->get_vehicle()->getAngleRelative());
 	}
 	else {
 		global_pos_x = lfveh->get_vehicle()->getPosition().x();
@@ -1987,7 +1987,7 @@ double lf_plugin_get_global_position_y(NumericalID veh_id) {
 
 	double global_pos_y = lfveh->get_vehicle()->getCachedGlobalPos().y() - (lfveh->get_vehicle()->getLength() / 2) * sin(lfveh->get_vehicle()->getAngleRelative());
 	if (lfveh->get_vehicle()->getGlobalCoordinatesControl()) {
-		global_pos_y = lfveh->get_vehicle()->getCachedGlobalPos().y();
+		return global_pos_y;
 	}
 	else {
 		global_pos_y = lfveh->get_vehicle()->getPosition().y();