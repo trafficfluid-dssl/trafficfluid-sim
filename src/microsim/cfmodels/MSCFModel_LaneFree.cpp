1a9166cc408867fe3a9bf4bb4c0926552767b18b
dtrou <d.troullinos@gmail.com>
2025-08-05 12:16:52 +0300

    minor fix, comments regarding licencing

--- src/microsim/cfmodels/MSCFModel_LaneFree.cpp
+++ src/microsim/cfmodels/MSCFModel_LaneFree.cpp
@@ -1,5 +1,8 @@
 /****************************************************************************/
-// This Source Code may be made available under the following 
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
 // Licenses when the conditions for such availability set forth in the Eclipse
 // Public License 2.0 are satisfied: GNU General Public License, version 2
 // or later which is available at
--- src/microsim/cfmodels/MSCFModel_LaneFree.h
+++ src/microsim/cfmodels/MSCFModel_LaneFree.h
@@ -1,12 +1,15 @@
 /****************************************************************************/
-// This Source Code may be made available under the following 
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
 // Licenses when the conditions for such availability set forth in the Eclipse
 // Public License 2.0 are satisfied: GNU General Public License, version 2
 // or later which is available at
 // https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 /****************************************************************************/
-/// @file    MSCFModel_LaneFree.cpp
+/// @file    MSCFModel_LaneFree.h
 /// @authors  Dimitrios Troullinos, Iason Chrysomallis
 /// @date    Thu, 29 Oct 2020
 ///
@@ -69,7 +72,6 @@ public:
     MSLaneFreeVehicle(MSVehicle* veh) : collided_with_cur{}, collided_with_prev{}{
         myveh = veh;
         speed_y = 0;
-        // veh->setSpeed(28);
         speed_x = veh->getSpeed();
         speed_x_desired = veh->getDesiredSpeed();
         
